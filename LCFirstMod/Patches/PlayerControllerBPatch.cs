16eeb96e50460670532e9db2a129270d899a4708
Toni G. <tty.gomez91@gmail.com>
2024-01-18 15:21:35 +0100

    init

---
+++ LCFirstMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LCFirstMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void InfiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ LCFirstMod/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCFirstMod.Patches;
+
+namespace LCFirstMod
+{
+    [BepInPlugin(ModGuid, ModName, ModVersion)]
+    public class LCMod : BaseUnityPlugin
+    {
+        private const string ModGuid = "Burstyx.LCFirstMod";
+        private const string ModName = "LC FirstMod";
+        private const string ModVersion = "1.0.0";
+
+        private readonly Harmony _harmony = new Harmony(ModGuid);
+
+        private static LCMod _instance;
+
+        private ManualLogSource _mls;
+
+        private void Awake()
+        {
+            if (_instance == null) _instance = this;
+
+            _mls = BepInEx.Logging.Logger.CreateLogSource(ModGuid);
+            
+            _mls.LogInfo("My first mod is loaded ^^!");
+            
+            _harmony.PatchAll(typeof(LCMod));
+            _harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
